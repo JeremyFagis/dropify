952696421d6b32d37b332c4a16302dfdb7ad3c95
Jeremy Fagis <jeremy@fagis.fr>
2015-06-16 17:33:47 +0200

    Refacto.

--- dist/js/dropify.js
+++ dist/js/dropify.js
@@ -18,8 +18,12 @@
 }(this, function($) {
 var pluginName = "dropify";
 
-function Dropify (element, options) {
-    defaults = {
+function Dropify(element, options) {
+    if (!(window.File && window.FileReader && window.FileList && window.Blob)) {
+        return;
+    }
+
+    var defaults = {
         defaultFile: '',
         maxFileSize: 0,
         messages: {
@@ -38,213 +42,223 @@ function Dropify (element, options) {
         }
     };
 
-    this.element        = element;
-    this.settings       = $.extend(true, defaults, options, $(this.element).data());
-    this._name          = pluginName;
-    this.imgFileFormats = ['png', 'jpg', 'jpeg', 'gif', 'bpm'],
-    this.file           = null,
-    this.filename       = null,
-    this.filenameElt    = null,
-    this.wrap           = null,
-    this.preview        = null,
-    this.isHTML5        = (window.File && window.FileReader && window.FileList && window.Blob) ? true : false;
-    this.isDisabled     = false,
+    this.element         = element;
+    this.input           = $(this.element);
+    this.wrapper         = null;
+    this.preview         = null;
+    this.filenameWrapper = null;
+    this.settings        = $.extend(true, defaults, options, this.input.data());
+    this.imgFileFormats  = ['png', 'jpg', 'jpeg', 'gif', 'bmp'];
+    this.file            = null;
+    this.filename        = null;
+    this.isDisabled      = false;
+
+    this.onChange = this.onChange.bind(this);
 
     this.translate();
-    this.init();
+    this.createElements();
+    this.setSize();
+
+    this.input.on('change', this.onChange);
 }
 
-Dropify.prototype = {
-    init: function () {
-        if (this.isHTML5) {
-            var _this = this;
+Dropify.prototype.onChange = function()
+{
+    this.resetPreview();
+    console.log("this.input", this.input.val());
+    // this.input.replaceWith(this.input.val('').clone(true));
+    // console.log("this.input", this.input.val());
 
-            _this.createElements();
-            _this.setSize();
+    // console.log('#input-file-now', $('#input-file-now').val());
+    // $('#input-file-now').val('');
+    // console.log('#input-file-now', $('#input-file-now').val());
 
-            $(this.element).on('change', function(){
-                _this.resetPreview();
-                _this.filename = _this.getFilename($(this).val());
-                _this.setFilename(_this.filename);
-                _this.readUrl(this);
-            });
-        }
-    },
-
-    createElements: function() {
-        var element = $(this.element),
-            defaultFile = this.settings.defaultFile || '';
-
-        element.wrap($(this.settings.tpl.wrap));
-        this.wrap = element.parent();
-
-        if (this.isTouchDevice() === true) {
-            this.wrap.addClass('touch-fallback');
-        }
-
-        if (element.attr('disabled')) {
-            this.isDisabled = true;
-            this.wrap.addClass('disabled');
-        }
-
-        var messageWrapper = $(this.settings.tpl.message).insertBefore(element);
-        this.errorElt = $(this.settings.tpl.error);
-        this.errorElt.appendTo(messageWrapper);
-
-        this.preview = $(this.settings.tpl.preview);
-        this.preview.insertAfter(element);
-
-        if (this.isDisabled === false) {
-            this.clearButton = $(this.settings.tpl.clearButton);
-            this.clearButton.insertAfter(this.element);
-
-            var _this = this;
-            this.clearButton.on('click', function(e){
-                _this.clearElement();
-            });
-        }
-
-        this.filenameElt = $(this.settings.tpl.filename);
-        this.filenameElt.prependTo(this.preview.find('.dropify-infos-inner'));
-
-        if (defaultFile.trim() != '') {
-            this.filename = defaultFile;
-            this.setPreview(defaultFile);
-            this.setFilename(this.getFilename(defaultFile));
-        }
-    },
-
-    readUrl: function(input) {
-        if (input.files && input.files[0]) {
-            var reader = new FileReader(),
-                _this = this;
-
-            this.file = input.files[0];
-
-            if (this.checkFileSize()) {
-                reader.onload = function(e) {
-                    _this.setPreview(e.target.result, _this.file.name);
-                }
-
-                reader.readAsDataURL(_this.file);
-            } else {
-                this.wrap.addClass('has-error');
-                this.resetPreview();
-                this.clearElement();
-            }
-        }
-    },
-
-    setPreview: function(src) {
-        this.wrap.removeClass('has-error').addClass('has-preview');
-        var render = this.preview.children('.dropify-render');
-
-        if (this.isImage() === true) {
-            $('<img />').attr('src', src).appendTo(render);
-        } else {
-            $('<i />').attr('class', 'dropify-font-file').appendTo(render);
-            $('<span class="dropify-extension" />').html(this.getFileType()).appendTo(render);
-        }
-
-        this.preview.fadeIn();
-    },
-
-    resetPreview: function() {
-        this.wrap.removeClass('has-preview');
-        var render = this.preview.children('.dropify-render');
-        render.find('.dropify-extension').remove();
-        render.find('i').remove();
-        render.find('img').remove();
-        this.preview.hide();
-    },
-
-    getFilename: function(src) {
-        var filename = src.split('\\').pop();
-        if (filename == src) {
-            filename = src.split('/').pop();
-        }
-
-        return src != "" ? filename : '';
-    },
-
-    setFilename: function(filename) {
-        this.filenameElt.children('.dropify-filename-inner').html(filename);
-    },
-
-    clearElement: function() {
-        this.file = null;
-        $(this.element).replaceWith($(this.element).val('').clone(true));
-        this.resetPreview();
-    },
-
-    setSize: function() {
-        if (this.settings.height) {
-            this.wrap.height(this.settings.height);
-        }
-    },
-
-    isTouchDevice: function() {
-        return (('ontouchstart' in window)
-             || (navigator.MaxTouchPoints > 0)
-             || (navigator.msMaxTouchPoints > 0));
-    },
-
-    getFileType: function() {
-        return this.filename.split('.').pop().toLowerCase();
-    },
-
-    isImage: function() {
-        var ext = this.getFileType();
-        if ($.inArray(ext, this.imgFileFormats) != "-1") {
-            return true;
-        }
-
-        return false;
-    },
-
-    translate: function() {
-        for (var name in this.settings.tpl) {
-            for (var key in this.settings.messages) {
-                this.settings.tpl[name] = this.settings.tpl[name].replace(key, this.settings.messages[key]);
-            }
-        }
-    },
-
-    checkFileSize: function() {
-        if (this.maxFileSizeToByte() === 0 || this.file.size <= this.maxFileSizeToByte()) {
-            return true;
-        }
-
-        return false;
-    },
-
-    maxFileSizeToByte: function() {
-        var value = 0;
-
-        if (this.settings.maxFileSize !== 0) {
-            var unit  = this.settings.maxFileSize.slice(-1).toUpperCase(),
-                kb    = 1024,
-                mb    = kb * 1024,
-                gb    = mb * 1024;
-
-            if (unit === 'K') {
-                value = parseFloat(this.settings.maxFileSize) * kb;
-            } else if (unit === 'M') {
-                value = parseFloat(this.settings.maxFileSize) * mb;
-            } else if (unit === 'G') {
-                value = parseFloat(this.settings.maxFileSize) * gb;
-            }
-        }
-
-        return value;
-    }
-
+    console.log('----------');
+    this.setFilename(this.input.val());
+    this.readUrl(this.element);
 };
 
-$.fn[ pluginName ] = function ( options ) {
+Dropify.prototype.createElements = function()
+{
+    var defaultFile = this.settings.defaultFile || '';
+
+    this.input.wrap($(this.settings.tpl.wrap));
+    this.wrapper = this.input.parent();
+
+    if (this.isTouchDevice() === true) {
+        this.wrapper.addClass('touch-fallback');
+    }
+
+    if (this.input.attr('disabled')) {
+        this.isDisabled = true;
+        this.wrapper.addClass('disabled');
+    }
+
+    var messageWrapper = $(this.settings.tpl.message).insertBefore(this.input);
+    this.errorElt = $(this.settings.tpl.error);
+    this.errorElt.appendTo(messageWrapper);
+
+    this.preview = $(this.settings.tpl.preview);
+    this.preview.insertAfter(this.input);
+
+    if (this.isDisabled === false) {
+        this.clearButton = $(this.settings.tpl.clearButton);
+        this.clearButton.insertAfter(this.input);
+        this.clearButton.on('click', function(e){
+            this.clearElement();
+        }.bind(this));
+    }
+
+    this.filenameWrapper = $(this.settings.tpl.filename);
+    this.filenameWrapper.prependTo(this.preview.find('.dropify-infos-inner'));
+
+    if (defaultFile.trim() != '') {
+        this.setFilename(defaultFile);
+        this.setPreview(defaultFile);
+    }
+};
+
+Dropify.prototype.readUrl = function(input)
+{
+    if (input.files && input.files[0]) {
+        var reader = new FileReader();
+        this.file = input.files[0];
+
+        if (this.checkFileSize()) {
+            reader.onload = function(e) {
+                this.setPreview(e.target.result, this.file.name);
+            }.bind(this);
+
+            reader.readAsDataURL(this.file);
+        } else {
+            this.wrapper.addClass('has-error');
+            this.resetPreview();
+            this.clearElement();
+        }
+    }
+};
+
+Dropify.prototype.setPreview = function(src)
+{
+    this.wrapper.removeClass('has-error').addClass('has-preview');
+    var render = this.preview.children('.dropify-render');
+
+    if (this.isImage() === true) {
+        $('<img />').attr('src', src).appendTo(render);
+    } else {
+        $('<i />').attr('class', 'dropify-font-file').appendTo(render);
+        $('<span class="dropify-extension" />').html(this.getFileType()).appendTo(render);
+    }
+
+    this.preview.fadeIn();
+};
+
+Dropify.prototype.resetPreview = function()
+{
+    this.wrapper.removeClass('has-preview');
+    var render = this.preview.children('.dropify-render');
+    render.find('.dropify-extension').remove();
+    render.find('i').remove();
+    render.find('img').remove();
+    this.preview.hide();
+};
+
+Dropify.prototype.getFilename = function(src)
+{
+    var filename = src.split('\\').pop();
+    if (filename == src) {
+        filename = src.split('/').pop();
+    }
+
+    return src != "" ? filename : '';
+};
+
+Dropify.prototype.setFilename = function(filename)
+{
+    var filename = this.getFilename(filename);
+    this.filename = filename;
+    this.filenameWrapper.children('.dropify-filename-inner').html(filename);
+};
+
+Dropify.prototype.clearElement = function()
+{
+    this.file = null;
+    this.input.replaceWith(this.input.val('').clone(true));
+    this.resetPreview();
+};
+
+Dropify.prototype.setSize = function()
+{
+    if (this.settings.height) {
+        this.wrapper.height(this.settings.height);
+    }
+};
+
+Dropify.prototype.isTouchDevice = function()
+{
+    return (('ontouchstart' in window)
+         || (navigator.MaxTouchPoints > 0)
+         || (navigator.msMaxTouchPoints > 0));
+};
+
+Dropify.prototype.getFileType = function()
+{
+    return this.filename.split('.').pop().toLowerCase();
+};
+
+Dropify.prototype.isImage = function()
+{
+    if (this.imgFileFormats.indexOf(this.getFileType()) != "-1") {
+        return true;
+    }
+
+    return false;
+};
+
+Dropify.prototype.translate = function()
+{
+    for (var name in this.settings.tpl) {
+        for (var key in this.settings.messages) {
+            this.settings.tpl[name] = this.settings.tpl[name].replace(key, this.settings.messages[key]);
+        }
+    }
+};
+
+Dropify.prototype.checkFileSize = function()
+{
+    if (this.maxFileSizeToByte() === 0 || this.file.size <= this.maxFileSizeToByte()) {
+        return true;
+    }
+
+    return false;
+};
+
+Dropify.prototype.maxFileSizeToByte = function()
+{
+    var value = 0;
+
+    if (this.settings.maxFileSize !== 0) {
+        var unit  = this.settings.maxFileSize.slice(-1).toUpperCase(),
+            kb    = 1024,
+            mb    = kb * 1024,
+            gb    = mb * 1024;
+
+        if (unit === 'K') {
+            value = parseFloat(this.settings.maxFileSize) * kb;
+        } else if (unit === 'M') {
+            value = parseFloat(this.settings.maxFileSize) * mb;
+        } else if (unit === 'G') {
+            value = parseFloat(this.settings.maxFileSize) * gb;
+        }
+    }
+
+    return value;
+};
+
+$.fn[pluginName] = function(options) {
     this.each(function() {
         if (!$.data(this, "plugin_" + pluginName)) {
-            $.data( this, "plugin_" + pluginName, new Dropify( this, options ) );
+            $.data(this, "plugin_" + pluginName, new Dropify(this, options));
         }
     });
 
